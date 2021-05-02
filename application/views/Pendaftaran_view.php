4e40cc08954ccc81def077bc3085454546c0b3d5
Dimasandhika97 <83528020+Dimasandhika97@users.noreply.github.com>
2021-05-02 12:45:50 +0700

    first commit

---
+++ application/views/Pendaftaran_view.php
@@ -0,0 +1,150 @@
+<div class="page-content sidebar-page right-sidebar-page clearfix">
+    <!-- .page-content-wrapper -->
+    <div class="page-content-wrapper">
+        <div class="page-content-inner">
+            <!-- Start .page-content-inner -->
+            <div id="page-header" class="clearfix">
+                <div class="page-header">
+                    <h2>FAKULTAS SAINS & INFORMATIKA</h2>
+                    <span class="txt">Simple form elements</span>
+                </div>
+                <div class="header-stats">
+                    <div class="spark clearfix">
+                        <div class="spark-info"><span class="number">2345</span>Visitors</div>
+                        <div id="spark-visitors" class="sparkline"></div>
+                    </div>
+                    <div class="spark clearfix">
+                        <div class="spark-info"><span class="number">17345</span>Views</div>
+                        <div id="spark-templateviews" class="sparkline"></div>
+                    </div>
+                    <div class="spark clearfix">
+                        <div class="spark-info"><span class="number">3700$</span>Sales</div>
+                        <div id="spark-sales" class="sparkline"></div>
+                    </div>
+                </div>
+            </div>
+            <!-- Start .row -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <!-- col-lg-12 start here -->
+                    <div class="panel panel-default toggle panelMove panelClose panelRefresh">
+                        <!-- Start .panel -->
+                        <div class="panel-heading">
+                            <h4 class="panel-title">Form elements</h4>
+                        </div>
+                        <div class="panel-body pt0 pb0">
+                            <form class="form-horizontal group-border stripped">
+                                <div class="form-group">
+                                    <label class="col-lg-2 col-md-3 control-label" for="">Nama</label>
+                                    <div class="col-lg-10 col-md-9">
+                                        <input type="text" class="form-control" name="default">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-lg-2 col-md-3 control-label" for="">Alamat Lengkap</label>
+                                    <div class="col-lg-10 col-md-9">
+                                        <input type="text" class="form-control" name="default">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-lg-2 col-md-3 control-label" for="">No.HP</label>
+                                    <div class="col-lg-10 col-md-9">
+                                        <input type="number" class="form-control" name="default">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-lg-2 col-md-3 control-label" for="">Tahun Lulus</label>
+                                    <div class="col-lg-10 col-md-9">
+                                        <input type="text" class="form-control" name="default">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-lg-2 col-md-3 control-label" for="">No.KTP</label>
+                                    <div class="col-lg-10 col-md-9">
+                                        <input type="number" class="form-control" name="default">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-lg-2 col-md-3 control-label" for="">Tanggal Lahir</label>
+                                    <div class="col-lg-10 col-md-9">
+                                        <input type="date" class="form-control" name="default">
+                                    </div>
+                                </div>
+                                 <div class="form-group">
+                                    <label class="col-lg-2 col-md-3 control-label" for="">Tempat Lahir</label>
+                                    <div class="col-lg-10 col-md-9">
+                                        <input type="text" class="form-control" name="default">
+                                    </div>
+                                </div>
+
+                                <!-- End .form-group  -->
+                                <div class="form-group">
+                                    <label class="col-lg-2 col-md-3 control-label">Jurusan</label>
+                                    <div class="col-lg-10 col-md-9">
+                                        <div class="radio-custom">
+                                            <input type="radio" name="radio" value="option1" id="radio1">
+                                            <label for="radio1">Teknik Informatik</label>
+                                        </div>
+                                        <div class="radio-custom">
+                                            <input type="radio" name="radio" value="option2" checked="checked" id="radio2">
+                                            <label for="radio2">Teknik Mesin</label>
+                                        </div>
+                                        <div class="radio-custom">
+                                            <input type="radio" name="radio" value="option2" checked="checked" id="radio2">
+                                            <label for="radio2">Teknik Industri</label>
+                                        </div>
+                                        <div class="radio-custom">
+                                            <input type="radio" name="radio" value="option2" checked="checked" id="radio2">
+                                            <label for="radio2">Teknik Elektro</label>
+                                        </div>
+                                        <div class="radio-custom">
+                                            <input type="radio" name="radio" value="option2" checked="checked" id="radio2">
+                                            <label for="radio2">Teknik Sipil</label>
+                                        </div>
+
+                                    </div>
+                                </div>
+                                <!-- End .form-group  -->
+                                <div class="form-group">
+                                    <label class="col-lg-2 col-md-3 control-label">Jenis Kelamin</label>
+                                    <div class="col-lg-10 col-md-9">
+                                        <div class="radio-custom radio-inline">
+                                            <input type="radio" name="radio1" value="option1" id="radio4">
+                                            <label for="radio4">Laki-laki</label>
+                                        </div>
+                                        <div class="radio-custom radio-inline">
+                                            <input type="radio" name="radio1" value="option2" checked="checked" id="radio5">
+                                            <label for="radio5">Perempuan</label>
+                                        </div>
+                                        <div class="form-group">
+                                                <div class="col-lg-offset-2">
+                                                    <button class="btn btn-default ml15" type="submit">CREATE</button>
+                                                </div>
+                                            </div>
+                                            <!-- End .form-group  -->
+                                       
+                                    </div>
+                                </div>
+
+
+                            </form>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+    <!-- End .panel -->
+</div>
+<!-- col-lg-12 end here -->
+
+<!-- End .row -->
+</div>
+<!-- End .page-content-inner -->
+</div>
+<!-- / page-content-wrapper -->
+</div>
